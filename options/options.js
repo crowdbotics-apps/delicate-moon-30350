9e03df3cff4e7d2929fdd7ffdcd5e2631e80bb30
Crowdbotics <team@crowdbotics.com>
2021-09-06 11:48:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "delicate_moon_30350Identifier",
+  url: "https://delicate_moon_30350Identifier.botics.co",
+  api: "https://delicate_moon_30350Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
